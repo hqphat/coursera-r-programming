85cbc5229d577e9b3f45836cc344e1589b013d5d
Eric Johnson <edj.boston@gmail.com>
2014-04-19 22:36:20 -0700

    :lipstick: cleaning up whitespace and return value

--- programming-assignment-1/complete.R
+++ programming-assignment-1/complete.R
@@ -6,22 +6,28 @@ complete <- function(directory, id = 1:332) {
     ## Get a list of filenames
     filenames <- list.files(path=directory, pattern="*.csv")
 
+    ## Initialize variables
     ids <-vector()
     counts = vector()
 
     ## Loop over the passed id's
     for(i in id) {
+
         ## Pad the i to create a filename
         filename = sprintf("%03d.csv", i)
         filepath <- paste(directory, filename, sep="/")
+
         ## Load the data
         data <- read.csv(filepath)
-        ##
+        
+        ## Store the id
         ids = c(ids, i)
+        
+        ## Calculate and store the count of complete cases
         completeCases = data[complete.cases(data),]
         counts = c(counts, nrow(completeCases))
     }
 
-    dt <- data.frame(id=ids, nobs=counts)
-    dt
+    ## Return the data frame
+    data.frame(id=ids, nobs=counts)
 }
