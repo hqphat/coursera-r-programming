52d130255d7355fc8601eaba36bbb17acb7d930f
Eric Johnson <edj.boston@gmail.com>
2014-04-19 22:35:56 -0700

    :lipstick: cleaning up whitespace

--- programming-assignment-1/pollutantmean.R
+++ programming-assignment-1/pollutantmean.R
@@ -13,15 +13,20 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 
         ## Loop over the passed id's
 		for(i in id) {
+
             ## Pad the i to create a filename
             filename = sprintf("%03d.csv", i)
             filepath <- paste(directory, filename, sep="/")
+
             ## Load the data
 			data <- read.csv(filepath)
+
             ## Select our column
             d <- data[,pollutant]
+
             ## Ignore NAs
 			d <- d[!is.na(d)]
+
             ## append to our vector
             vals = c(vals, d)
 		}
